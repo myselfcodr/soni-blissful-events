14bf7fc69accfadb54f2802767fc2dd7a7dfc217
ashishnsd <ashishplusag@gmail.com>
2025-10-02 04:17:38 +0530

    updated axoiosInstance.js with autoswitch use local or netlify

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000", // Replace with your backend URL
+  baseURL: import.meta.env.VITE_API_URL, // Replace with your backend URL
 });
 
 // Request interceptor to add JWT token automatically
