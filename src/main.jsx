587f5eec04f90d6d1f91a0e4aedf6ab02c925158
ASHISH NISHAD <133517062+myselfcodr@users.noreply.github.com>
2025-11-09 19:45:11 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { RouterProvider } from "react-router";
+import { RouterProvider } from "react-router-dom";
 import Router from "./Router/Router";
 import AuthProvider from "./Provider/AuthProvider";
 import { ToastContainer } from "react-toastify";
